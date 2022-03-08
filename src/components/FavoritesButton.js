46bdd7e37fbeca3eab8da3e261125a16eaf767be
Steven Bui <bui.stvn@gmail.com>
2022-03-08 15:31:14 -0800

    Add toggled state to buttons

--- src/components/FavoritesButton.js
+++ src/components/FavoritesButton.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '@chakra-ui/react';
 import { FaHeart, FaRegHeart } from 'react-icons/fa';
 
 const FavoritesButton = () => {
+    // TODO: Fetch "added" state from API instead after user is implemented
+    const [addedToList, setAddedToList] = useState(false);
+
+    const handleClick = () => {
+        setAddedToList(!addedToList);
+    };
+
     return (
-        <Button colorScheme="red" variant="outline" leftIcon={<FaRegHeart />}>
-            Add to Favorites
-        </Button>
+        <>
+            {!addedToList ? (
+                <Button
+                    colorScheme="red"
+                    variant="outline"
+                    leftIcon={<FaRegHeart />}
+                    onClick={handleClick}
+                >
+                    Add to Favorites
+                </Button>
+            ) : (
+                <Button
+                    colorScheme="red"
+                    variant="solid"
+                    leftIcon={<FaHeart />}
+                    onClick={handleClick}
+                >
+                    Added
+                </Button>
+            )}
+        </>
     );
 };
 
--- src/components/WatchlistButton.js
+++ src/components/WatchlistButton.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '@chakra-ui/react';
 import { FaListAlt, FaRegListAlt } from 'react-icons/fa';
 
 const WatchlistButton = () => {
+    // TODO: Fetch "added" state from API instead after user is implemented
+    const [addedToList, setAddedToList] = useState(false);
+
+    const handleClick = () => {
+        setAddedToList(!addedToList);
+    };
+
     return (
-        <Button
-            colorScheme="green"
-            variant="outline"
-            leftIcon={<FaRegListAlt />}
-        >
-            Add to Watchlist
-        </Button>
+        <>
+            {!addedToList ? (
+                <Button
+                    colorScheme="green"
+                    variant="outline"
+                    leftIcon={<FaRegListAlt />}
+                    onClick={handleClick}
+                >
+                    Add to Watchlist
+                </Button>
+            ) : (
+                <Button
+                    colorScheme="green"
+                    variant="solid"
+                    leftIcon={<FaListAlt />}
+                    onClick={handleClick}
+                >
+                    Added
+                </Button>
+            )}
+        </>
     );
 };
 
