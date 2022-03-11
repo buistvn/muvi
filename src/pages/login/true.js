0d03b6e7b2e3eea38316b40f6ed8221a6bd84002
Alex Young <younga6@oregonstate.edu>
2022-03-10 22:01:17 -0800

    fetched and stored account info

--- src/pages/login/true.js
+++ src/pages/login/true.js
@@ -1,8 +1,8 @@
 import { Text } from '@chakra-ui/react';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { UserContext } from '../_app';
 
-const True = ({ user }) => {
+const True = () => {
     const {
         user,
         setUser,
@@ -13,8 +13,22 @@ const True = ({ user }) => {
         name,
         setName,
     } = useContext(UserContext);
-    console.log(sessionID);
-    return <Text>Logged in Successfully</Text>;
+    useEffect(() => {
+        async function fetchData() {
+            const res = await fetch(
+                `https://api.themoviedb.org/3/account?api_key=${process.env.NEXT_PUBLIC_API_KEY}&session_id=${sessionID}`
+            );
+            if (res.status === 401) {
+                console.log('== Error: No Token');
+            } else {
+                const body = await res.json();
+                setAccountID(body.id);
+                setName(body.username);
+            }
+        }
+        fetchData();
+    }, []);
+    return <Text>Logged in Successfully: {name}</Text>;
 };
 
 export default True;
